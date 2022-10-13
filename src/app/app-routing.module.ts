a6975bc0621bf11da209ad6efb0e3ddeb0c16f6a
MANUEL <manuelaaragon95@gmail.com>
2022-10-13 18:29:03 -0500

    cambio de version

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -22,7 +22,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [
-    RouterModule.forRoot( routes ),
+    RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' }),
     PagesRoutingModule,
     AuthRoutingModule
   ],
